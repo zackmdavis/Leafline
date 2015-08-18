97312f17677a101af954e76dd788346d6be1699c
Zack M. Davis <code@zackmdavis.net>
2015-08-17 22:21:02 -0700

    namespaces are a honking great idea and we should do more of them

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,11 @@
 extern crate ansi_term;
+mod space;
 mod movement_tables;
 
 use ansi_term::Colour as Color;  // this is America
 
+use space::{Locale, Bitboard};
 use movement_tables::PONY_MOVEMENT_TABLE;
-
-
-#[derive(Eq,PartialEq,Debug,Copy,Clone)]
-struct Locale {
-    rank: u8,
-    file: u8
-}
-
-impl Locale {
-    pub fn bit_index(&self) -> u32 {
-        (8u32 * self.rank as u32) + self.file as u32
-    }
-
-    pub fn pinpoint(&self) -> Bitboard {
-        Bitboard(2u64.pow(self.bit_index()))
-    }
-
-    pub fn is_legal(&self) -> bool {
-        self.rank >= 0 && self.rank < 8 && self.file >= 0 && self.file < 8
-    }
-
-    pub fn displace(&self, offset: (i8, i8)) -> Option<Self> {
-        let (rank_offset, file_offset) = offset;
-        let potential_locale = Locale {
-            // XXX: it won't happen with the arguments we expect to
-            // give it in this program, but in the interests of Safety,
-            // this is an overflow bug (-1i8 as u8 == 255u8)
-            rank: (self.rank as i8 + rank_offset) as u8,
-            file: (self.file as i8 + file_offset) as u8
-        };
-        if potential_locale.is_legal() {
-            Some(potential_locale)
-        } else {
-            None
-        }
-    }
-}
-
-
-
-#[derive(Eq,PartialEq,Debug,Copy,Clone)]
-struct Bitboard(u64);
-
-impl Bitboard {
-    pub fn new() -> Bitboard {
-        Bitboard(0)
-    }
-
-    pub fn init(starters: &Vec<Locale>) -> Bitboard {
-        let mut board = Bitboard::new();
-        for &starter in starters.iter() {
-            board = board.alight(starter);
-        }
-        board
-    }
-
-    pub fn union(&self, other: Bitboard) -> Bitboard {
-        let Bitboard(our_bits) = *self;
-        let Bitboard(their_bits) = other;
-        Bitboard(our_bits | their_bits)
-    }
-
-    pub fn intersection(&self, other: Bitboard) -> Bitboard {
-        let Bitboard(our_bits) = *self;
-        let Bitboard(their_bits) = other;
-        Bitboard(our_bits & their_bits)
-    }
-
-    pub fn invert(&self) -> Bitboard {
-        let Bitboard(our_bits) = *self;
-        Bitboard(!our_bits)
-    }
-
-    pub fn alight(&self, station: Locale) -> Bitboard {
-        // classic `|= (1 << n)` would probably be more efficient, huh &c.
-        self.union(station.pinpoint())
-    }
-
-    pub fn quench(&self, station: Locale) -> Bitboard {
-        self.intersection(station.pinpoint().invert())
-    }
-
-    pub fn transit(&self, departure: Locale, destination: Locale) -> Bitboard {
-        self.quench(departure).alight(destination)
-    }
-
-    pub fn query(&self, station: Locale) -> bool {
-        let Bitboard(our_bits) = *self;
-        let Bitboard(beacon_bits) = station.pinpoint();
-        (our_bits & beacon_bits) != 0
-    }
-
-    pub fn to_locales(&self) -> Vec<Locale> {
-        let mut locales = Vec::new();
-        for rank in 0..8 {
-            for file in 0..8 {
-                let potential_locale = Locale { rank: rank, file: file };
-                if self.query(potential_locale) {
-                    locales.push(potential_locale);
-                }
-            }
-        }
-        locales
-    }
-
-    pub fn display(&self) {
-        let Bitboard(debug) = *self;
-        println!("{}", debug);
-        for rank in 0..8 {
-            for file in 0..8 {
-                if self.query(Locale { rank: rank, file: file }) {
-                    print!("• ");
-                } else {
-                    print!("_ ");
-                }
-            }
-            println!("");
-        }
-    }
-}
 
 
 #[derive(Eq,PartialEq,Debug,Copy,Clone)]
@@ -504,69 +386,8 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use super::{Locale, Bitboard, Team, JobDescription, Agent, GameState};
-
-    #[test]
-    fn test_locale() {
-        let a1 = Locale { rank: 0, file: 0 };
-        assert_eq!(Bitboard(1u64), a1.pinpoint());
-
-        let c4 = Locale { rank: 2, file: 3 };
-        assert_eq!(Bitboard(524288u64), c4.pinpoint());
-    }
-
-    #[test]
-    fn test_displace_locale() {
-        assert_eq!(
-            Some(Locale { rank: 3, file: 5 }),
-            Locale { rank: 4, file: 4 }.displace((-1, 1))
-        );
-        assert_eq!(
-            None,
-            Locale { rank: 0, file: 0 }.displace((-1, 1))
-        )
-    }
-
-    #[test]
-    fn test_empty_board_is_empty() {
-        let empty_board = Bitboard::new();
-        for rank in 0..8 {
-            for file in 0..8 {
-                assert!(!empty_board.query(Locale { rank: rank, file: file }));
-            }
-        }
-    }
-
-    #[test]
-    fn test_inverted_empty_board_is_full() {
-        let full_board = Bitboard::new().invert();
-        for rank in 0..8 {
-            for file in 0..8 {
-                assert!(full_board.query(Locale { rank: rank, file: file }));
-            }
-        }
-    }
-
-    #[test]
-    fn test_alight_and_quench() {
-        let mut stage = Bitboard(0);
-        let b5 = Locale { rank: 1, file: 4 };
-        stage = stage.alight(b5);
-        assert!(stage.query(b5));
-        stage = stage.quench(b5);
-        assert!(!stage.query(b5));
-    }
-
-    #[test]
-    fn test_init_bitboard() {
-        let starters = vec![Locale{ rank: 1, file: 2 },
-                            Locale{ rank: 3, file: 4 },
-                            Locale{ rank: 5, file: 6 }];
-        let stage = Bitboard::init(&starters);
-        for &starter in starters.iter() {
-            assert!(stage.query(starter));
-        }
-    }
+    use super::{Team, JobDescription, Agent, GameState};
+    use space::{Locale, Bitboard};
 
     #[test]
     fn test_agent_to_bitboard_ref_on_new_gamestate() {
---
+++ src/space.rs
@@ -0,0 +1,166 @@
+#[derive(Eq,PartialEq,Debug,Copy,Clone)]
+pub struct Locale {
+    pub rank: u8,
+    pub file: u8
+}
+
+impl Locale {
+    pub fn bit_index(&self) -> u32 {
+        (8u32 * self.rank as u32) + self.file as u32
+    }
+
+    pub fn pinpoint(&self) -> Bitboard {
+        Bitboard(2u64.pow(self.bit_index()))
+    }
+
+    pub fn is_legal(&self) -> bool {
+        self.rank >= 0 && self.rank < 8 && self.file >= 0 && self.file < 8
+    }
+
+    pub fn displace(&self, offset: (i8, i8)) -> Option<Self> {
+        let (rank_offset, file_offset) = offset;
+        let potential_locale = Locale {
+            // XXX: it won't happen with the arguments we expect to
+            // give it in this program, but in the interests of Safety,
+            // this is an overflow bug (-1i8 as u8 == 255u8)
+            rank: (self.rank as i8 + rank_offset) as u8,
+            file: (self.file as i8 + file_offset) as u8
+        };
+        if potential_locale.is_legal() {
+            Some(potential_locale)
+        } else {
+            None
+        }
+    }
+}
+
+
+#[derive(Eq,PartialEq,Debug,Copy,Clone)]
+pub struct Bitboard(pub u64);
+
+impl Bitboard {
+    pub fn new() -> Bitboard {
+        Bitboard(0)
+    }
+
+    pub fn init(starters: &Vec<Locale>) -> Bitboard {
+        let mut board = Bitboard::new();
+        for &starter in starters.iter() {
+            board = board.alight(starter);
+        }
+        board
+    }
+
+    pub fn union(&self, other: Bitboard) -> Bitboard {
+        let Bitboard(our_bits) = *self;
+        let Bitboard(their_bits) = other;
+        Bitboard(our_bits | their_bits)
+    }
+
+    pub fn intersection(&self, other: Bitboard) -> Bitboard {
+        let Bitboard(our_bits) = *self;
+        let Bitboard(their_bits) = other;
+        Bitboard(our_bits & their_bits)
+    }
+
+    pub fn invert(&self) -> Bitboard {
+        let Bitboard(our_bits) = *self;
+        Bitboard(!our_bits)
+    }
+
+    pub fn alight(&self, station: Locale) -> Bitboard {
+        // classic `|= (1 << n)` would probably be more efficient, huh &c.
+        self.union(station.pinpoint())
+    }
+
+    pub fn quench(&self, station: Locale) -> Bitboard {
+        self.intersection(station.pinpoint().invert())
+    }
+
+    pub fn transit(&self, departure: Locale, destination: Locale) -> Bitboard {
+        self.quench(departure).alight(destination)
+    }
+
+    pub fn query(&self, station: Locale) -> bool {
+        let Bitboard(our_bits) = *self;
+        let Bitboard(beacon_bits) = station.pinpoint();
+        (our_bits & beacon_bits) != 0
+    }
+
+    pub fn to_locales(&self) -> Vec<Locale> {
+        let mut locales = Vec::new();
+        for rank in 0..8 {
+            for file in 0..8 {
+                let potential_locale = Locale { rank: rank, file: file };
+                if self.query(potential_locale) {
+                    locales.push(potential_locale);
+                }
+            }
+        }
+        locales
+    }
+
+    pub fn display(&self) {
+        let Bitboard(debug) = *self;
+        println!("{}", debug);
+        for rank in 0..8 {
+            for file in 0..8 {
+                if self.query(Locale { rank: rank, file: file }) {
+                    print!("• ");
+                } else {
+                    print!("_ ");
+                }
+            }
+            println!("");
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::{Locale, Bitboard};
+
+    #[test]
+    fn test_locale() {
+        let a1 = Locale { rank: 0, file: 0 };
+        assert_eq!(Bitboard(1u64), a1.pinpoint());
+
+        let c4 = Locale { rank: 2, file: 3 };
+        assert_eq!(Bitboard(524288u64), c4.pinpoint());
+    }
+
+    #[test]
+    fn test_displace_locale() {
+        assert_eq!(
+            Some(Locale { rank: 3, file: 5 }),
+            Locale { rank: 4, file: 4 }.displace((-1, 1))
+        );
+        assert_eq!(
+            None,
+            Locale { rank: 0, file: 0 }.displace((-1, 1))
+        )
+    }
+
+    #[test]
+    fn test_alight_and_quench() {
+        let mut stage = Bitboard(0);
+        let b5 = Locale { rank: 1, file: 4 };
+        stage = stage.alight(b5);
+        assert!(stage.query(b5));
+        stage = stage.quench(b5);
+        assert!(!stage.query(b5));
+    }
+
+    #[test]
+    fn test_init_bitboard() {
+        let starters = vec![Locale{ rank: 1, file: 2 },
+                            Locale{ rank: 3, file: 4 },
+                            Locale{ rank: 5, file: 6 }];
+        let stage = Bitboard::init(&starters);
+        for &starter in starters.iter() {
+            assert!(stage.query(starter));
+        }
+    }
+
+}
