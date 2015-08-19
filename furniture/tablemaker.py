f936f6d94e0b8f0429b6bfb1dbdbc065d1cd42a8
Zack M. Davis <code@zackmdavis.net>
2015-08-18 22:29:32 -0700

    "Doesn't seem right, to take information, given at close range"

--- furniture/tablemaker.py
+++ furniture/tablemaker.py
@@ -19,14 +19,21 @@ def is_legal(position):
 PONY_OPTIONS = ((+1, +2), (-1, +2), (+1, -2), (-1, -2),
                 (+2, +1), (-2, +1), (+2, -1), (-2, -1))
 
-PONY_RESULT = [reduce(operator.ior,
-                     [rank_and_file_to_u64(displace(position, offset))
-                      for offset in PONY_OPTIONS
-                      if is_legal(displace(position, offset))])
-              for position in itertools.product(range(8), repeat=2)]
+FIGUREHEAD_OPTIONS = [(i, j)
+                      for i in (-1, 0, 1)
+                      for j in (-1, 0, 1)
+                      if not i == j == 0]
 
-def embeddable_result(result):
-    return "pub static PONY_MOVEMENT_TABLE: [u64; 64] = [\n{}\n];\n".format(
+def universal_distribution(options):
+    return [reduce(operator.ior,
+                   [rank_and_file_to_u64(displace(position, offset))
+                    for offset in options
+                    if is_legal(displace(position, offset))])
+            for position in itertools.product(range(8), repeat=2)]
+
+def the_book_of_life(job_description, result):
+    return "pub static {}_MOVEMENT_TABLE: [u64; 64] = [\n{}\n];\n".format(
+        job_description.upper(),
         '\n'.join("    {},".format(entry) for entry in result)
     )
 
@@ -34,5 +41,12 @@ def embeddable_result(result):
 if __name__ == "__main__":
     with open(os.path.join('src', 'movement_tables.rs'),
               'w') as movement_tables_rs:
-        movement_tables_rs.write(embeddable_result(PONY_RESULT))
+        movement_tables_rs.write(
+            '\n\n'.join(
+                [the_book_of_life(
+                    "pony", universal_distribution(PONY_OPTIONS)),
+                 the_book_of_life(
+                     "figurehead", universal_distribution(FIGUREHEAD_OPTIONS))]
+            )
+        )
     print("Wrote movement_tables.rs!")
