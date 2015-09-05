4eab80f79798b211c835efab5fe2e5331a955f7e
Zack M. Davis <code@zackmdavis.net>
2015-09-05 14:55:38 -0700

    playable web client!!

--- tasks.py
+++ tasks.py
@@ -1,13 +1,15 @@
 import os
+import subprocess
 from urllib.request import urlretrieve
 
 from invoke import task, run
 
+# TODO: current-working-directory robustness
 
 @task
 def install_chessboard_js():
     urlretrieve("http://chessboardjs.com/releases/0.3.0/chessboardjs-0.3.0.zip",
-                # TODO: current-working-directory robustness
+
                 os.path.join('web_client', 'resources',
                              'chessboardjs-0.3.0.zip'))
     # TODO: unzip JS and CSS to appropriate places
@@ -16,3 +18,13 @@ def install_chessboard_js():
 @task
 def download_statics():
     install_chessboard_js()
+
+
+@task
+def compile_client():
+    subprocess.check_output(
+        ["babel",
+         "web_client/resources/public/js/leafline_client.js",
+         "--out-file",
+         "web_client/resources/public/js/leafline_client-built.js"]
+    )
--- web_client/src/leafline_web_client/routes/home.clj
+++ web_client/src/leafline_web_client/routes/home.clj
@@ -3,7 +3,8 @@
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
             [clojure.java.io :as io]
-            [clojure.java.shell :refer [sh]]))
+            [clojure.java.shell :refer [sh]]
+            [taoensso.timbre :as timbre]))
 
 (defn home-page []
   (layout/render "home.html"))
@@ -12,13 +13,21 @@
   (layout/render "about.html"))
 
 (defn correspondence-endpoint [request]
-  (let [world (get-in request [:params :world])
-        dictation ((sh "./Leafline"
-                       "--lookahead" "4"
-                       "--correspond" world) :out)]
-    {:status 200
-     :headers {"Content-Type" "application/json"}
-     :body dictation}))
+  ;; TODO validate world, 400 if bad
+  (let [world (str (get-in request [:params :world])
+                   " b")  ; the AI plays blue (by convention, for now)
+        mail-call ["./Leafline" "--lookahead" "4" "--correspond" world]]
+    (timbre/info "got postcard about " world
+                 "; invoking Leafline with" mail-call)
+    (let [dictation (apply sh mail-call)]
+      (if (zero? (dictation :exit))
+        {:status 200
+         ;; TODO RESEARCH: JSON helpers
+         :headers {"Content-Type" "application/json"}
+         :body (dictation :out)}
+        {:status 500
+         :headers {"Content-Type" "application/json"}
+         :body {:error (dictation :err)}}))))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
