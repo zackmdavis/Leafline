972a1eb6a5bf77c114e1ff32174e597f1e188c4b
Zack M. Davis <code@zackmdavis.net>
2015-09-07 17:26:19 -0700

    bump version and web client AI depth; note new things in README

--- web_client/project.clj
+++ web_client/project.clj
@@ -1,4 +1,4 @@
-(defproject leafline-web-client "0.0.8"
+(defproject leafline-web-client "0.0.9"
 
   :description "Web client for the Leafline oppositional strategy game engine"
   :url "https://github.com/zackmdavis/Leafline"
