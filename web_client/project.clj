9407681451e7ce7ef1e0540d65b6c76d64a8be03
Zack M. Davis <code@zackmdavis.net>
2015-09-20 21:02:29 -0700

    bump

--- web_client/project.clj
+++ web_client/project.clj
@@ -1,4 +1,4 @@
-(defproject leafline-web-client "0.0.12"
+(defproject leafline-web-client "0.0.13"
 
   :description "Web client for the Leafline oppositional strategy game engine"
   :url "https://github.com/zackmdavis/Leafline"
