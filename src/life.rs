zackmdavis/Leafline

commit c04afe38a1e62d5b6a13ea4419dde06edd9fb635

Save some space in the Locale
doesnt obviously affect performance, but does increase how many entries
the cache can store, so it must be better... right?!

src/space.rs
@@ -1,11 +1,10 @@
 #[derive(Eq,PartialEq,Debug,Copy,Clone,Hash,RustcEncodable,RustcDecodable)]
 pub struct Locale {
-    rank: u8,
-    file: u8,
+    rank_and_file: u8,
 }
 
-pub const ORANGE_FIGUREHEAD_START: Locale = Locale { rank: 0, file: 4 };
-pub const BLUE_FIGUREHEAD_START: Locale = Locale { rank: 7, file: 4 };
+pub const ORANGE_FIGUREHEAD_START: Locale = Locale { rank_and_file: (0 << 4) | 4 };
+pub const BLUE_FIGUREHEAD_START: Locale = Locale { rank_and_file: (7 << 4) | 4 };
 
 static INDEX_TO_FILE_NAME: [char; 8] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
@@ -13,12 +12,13 @@ static INDEX_TO_FILE_NAME: [char; 8] = [
 
 lazy_static! {
     static ref LOCALE_STASH: [Locale; 64] = {
-        let mut m: [Locale; 64] = [Locale {rank: 0, file: 0}; 64];
+        let mut m: [Locale; 64] = [Locale {rank_and_file: 0}; 64];
         for rank in 0..8 {
             for file in 0..8 {
                 let idx = (rank*8 + file) as usize;
 
-                m[idx] = Locale { rank, file };
+                let rank_and_file = (rank << 4) | file;
+                m[idx] = Locale { rank_and_file }
             }
         }
         m
@@ -33,7 +33,7 @@ impl Locale {
     }
 
     pub fn to_algebraic(&self) -> String {
-        format!("{}{}", INDEX_TO_FILE_NAME[self.file as usize], self.rank() + 1)
+        format!("{}{}", INDEX_TO_FILE_NAME[self.file() as usize], self.rank() + 1)
     }
 
     pub fn from_algebraic(notation: &str) -> Self {
@@ -49,7 +49,7 @@ impl Locale {
     }
 
     pub fn pindex(&self) -> u32 {
-        (8u32 * u32::from(self.rank())) + u32::from(self.file)
+        (8u32 * u32::from(self.rank())) + u32::from(self.file())
     }
 
     pub fn pinpoint(&self) -> Pinfield {
@@ -57,19 +57,24 @@ impl Locale {
     }
 
     pub fn is_legal(&self) -> bool {
-        self.rank() < 8 && self.file < 8
+        self.rank() < 8 && self.file() < 8
+    }
+
+    fn build_possibly_illegal(rank: u8, file: u8) -> Self {
+        let rank_and_file = (rank << 4) | file;
+        Locale { rank_and_file }
     }
 
     pub fn displace(&self, offset: (i8, i8)) -> Option<Self> {
         let (rank_offset, file_offset) = offset;
         // note: when constructing possibly-illegal Locales, do not use ::new
-        let potential_locale = Locale {
+        let potential_locale = Locale::build_possibly_illegal(
             // XXX: it won't happen with the arguments we expect to
             // give it in this program, but in the interests of Safety,
             // this is an overflow bug (-1i8 as u8 == 255u8)
-            rank: (self.rank() as i8 + rank_offset) as u8,
-            file: (self.file() as i8 + file_offset) as u8,
-        };
+            (self.rank() as i8 + rank_offset) as u8,
+            (self.file() as i8 + file_offset) as u8,
+        );
         if potential_locale.is_legal() {
             Some(potential_locale)
         } else {
@@ -83,11 +88,11 @@ impl Locale {
                                       factor * file_offset);
 
         // note: when constructing possibly-illegal Locales, do not use ::new
-        let potential_locale = Locale {
+        let potential_locale = Locale::build_possibly_illegal(
             // XXX: could overflow given unrealistic arguments
-            rank: (self.rank() as i8 + real_rank) as u8,
-            file: (self.file() as i8 + real_file) as u8,
-        };
+            (self.rank() as i8 + real_rank) as u8,
+            (self.file() as i8 + real_file) as u8,
+        );
         if potential_locale.is_legal() {
             Some(potential_locale)
         } else {
@@ -96,11 +101,11 @@ impl Locale {
     }
 
     pub fn rank(&self) -> u8 {
-        self.rank
+        self.rank_and_file >> 4
     }
 
     pub fn file(&self) -> u8 {
-        self.file
+        self.rank_and_file & (0b1111)
     }
 }
 
@@ -341,6 +346,20 @@ mod tests {
                        // TODO: again, conversion in iterator
                        Locale::from_algebraic(*actuality));
         }
+    }
+
+    #[test]
+    fn concerning_all_locales() {
+        for rank in 0..8 {
+            for file in 0..8 {
+                let l = Locale {
+                    rank_and_file: (rank << 4) | file
+                };
+                assert_eq!(rank, l.rank());
+                assert_eq!(file, l.file());
+            }
+        }
+
     }
 
     #[test]

commit 26ad01d0961345d06249eb258a10f16623c952e5

don't save space in correspondence postcards
The space-saving measures in c04afe38a1 broke the web client. (It was
possible to make the first move, but it got confused immediately
thereafter because the client couldn't understand the compressed
`rank_and_file` field now being sent from the game engine.) I'm less
afraid of editing five-year-old Rust than JavaScript, so let's slap in
some `From`/`Into` conversions to make the JSON output be the way it
used to rather than updating the client.

src/life.rs
@@ -2,7 +2,7 @@
 use std::default::Default;
 use std::fmt;
 
-use space::{Locale, Pinfield, ORANGE_FIGUREHEAD_START, BLUE_FIGUREHEAD_START};
+use space::{Locale, RelaxedLocale, Pinfield, ORANGE_FIGUREHEAD_START, BLUE_FIGUREHEAD_START};
 use identity::{Agent, JobDescription, Team};
 use motion::{FIGUREHEAD_MOVEMENT_TABLE, PONY_MOVEMENT_TABLE};
 use ansi_term::Colour as Color;
@@ -17,6 +17,23 @@ pub struct Patch {
     pub star: Agent,
     pub whence: Locale,
     pub whither: Locale,
+}
+
+#[derive(Eq,PartialEq,Debug,Copy,Clone,Hash,RustcEncodable,RustcDecodable)]
+pub struct TransitPatch {
+    pub star: Agent,
+    pub whence: RelaxedLocale,
+    pub whither: RelaxedLocale,
+}
+
+impl From<Patch> for TransitPatch {
+    fn from(patch: Patch) -> Self {
+        Self {
+            star: patch.star,
+            whence: patch.whence.into(),
+            whither: patch.whither.into(),
+        }
+    }
 }
 
 impl Patch {

src/main.rs
@@ -45,7 +45,7 @@ use rustc_serialize::json;
 use time::{Duration, get_time};
 
 use identity::{Agent, Team};
-use life::{Commit, Patch, WorldState};
+use life::{Commit, Patch, TransitPatch, WorldState};
 use mind::{Variation, fixed_depth_sequence_kickoff, iterative_deepening_kickoff,
            kickoff, pagan_variation_format, Memory};
 use substrate::memory_free;
@@ -182,11 +182,11 @@ fn forecast<T: 'static + Memory>(world: WorldState, bound: LookaheadBound, déj�
 #[derive(RustcEncodable, RustcDecodable)]
 struct Postcard {
     world: String,
-    patch: Patch,
+    patch: TransitPatch,
     hospitalization: Option<Agent>,
     thinking_time: u64,
     depth: u8,
-    counterreplies: Vec<Patch>,
+    counterreplies: Vec<TransitPatch>,
     rosetta_stone: String,
 }
 
@@ -211,7 +211,7 @@ fn correspondence(reminder: &str, bound: LookaheadBound, déjà_vu_bound: f32)
         let counterreplies = determination.tree
                                           .lookahead()
                                           .iter()
-                                          .map(|c| c.patch)
+                                          .map(|c| TransitPatch::from(c.patch))
                                           .collect::<Vec<_>>();
         if counterreplies.is_empty() {
             if determination.tree.in_critical_endangerment(Team::Orange) {
@@ -225,7 +225,7 @@ fn correspondence(reminder: &str, bound: LookaheadBound, déjà_vu_bound: f32)
         }
         let postcard = Postcard {
             world: determination.tree.preserve(),
-            patch: determination.patch,
+            patch: TransitPatch::from(determination.patch),
             hospitalization: determination.hospitalization,
             thinking_time: sidereal.num_milliseconds() as u64,
             depth,

src/space.rs
@@ -3,6 +3,20 @@ pub struct Locale {
     rank_and_file: u8,
 }
 
+#[derive(Eq,PartialEq,Debug,Copy,Clone,Hash,RustcEncodable,RustcDecodable)]
+pub struct RelaxedLocale {
+    rank: u8,
+    file: u8,
+}
+
+// A less-compressed representation of a Locale, for compatibility with the
+// legacy web client
+impl From<Locale> for RelaxedLocale {
+    fn from(locale: Locale) -> Self {
+        Self { rank: locale.rank(), file: locale.file() }
+    }
+}
+
 pub const ORANGE_FIGUREHEAD_START: Locale = Locale { rank_and_file: (0 << 4) | 4 };
 pub const BLUE_FIGUREHEAD_START: Locale = Locale { rank_and_file: (7 << 4) | 4 };
 